4b299bd7e3149ed8da3f45d524c9a7b34b5c71a3
Ilija Zikic <zikicilija@gmail.com>
2025-07-25 13:49:24 +0200

    Add files via upload

---
+++ src/js/form.js
@@ -0,0 +1,6 @@
+const submit = document.querySelector(".form__submit");
+
+submit.addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log(e);
+});
--- src/js/main.js
+++ src/js/main.js
@@ -11,24 +11,3 @@ function toggleMenu() {
 
 hamburger.addEventListener("click", toggleMenu);
 close.addEventListener("click", toggleMenu);
-
-// FORM
-const submit = document.querySelector(".form__submit");
-
-submit.addEventListener("click", function (e) {
-  e.preventDefault();
-  console.log(e);
-});
-
-// MAP
-var map = L.map("map").setView([44.8873868858456, 20.45131738180942], 15);
-
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
-
-L.marker([44.8873868858456, 20.45131738180942])
-  .addTo(map)
-  .bindPopup("Home")
-  .openPopup();
