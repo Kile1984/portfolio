7f7508ab7e686e75d4f24c856c4e59e5c95e128c
Ilija Zikic <zikicilija@gmail.com>
2025-07-31 23:55:14 +0200

    Add files via upload

--- src/js/form.js
+++ src/js/form.js
@@ -1,6 +1,11 @@
 const submit = document.querySelector(".form__submit");
+const inputs = document.querySelectorAll(".form__input");
+const textarea = document.querySelector(".form__textarea");
 
 submit.addEventListener("click", function (e) {
   e.preventDefault();
-  console.log(e);
+
+  [...inputs].map((i) => {
+    console.log(i);
+  });
 });
